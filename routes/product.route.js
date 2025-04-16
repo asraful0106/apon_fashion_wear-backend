bbc9cc2222a6f36943cb78005384cb27b5c48d45
Md. Asraful Alom <asrafulapf@gmail.com>
2025-04-16 09:39:17 +0600

    route for update, delete hase been added

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,16 +1,26 @@
 import { Router } from 'express';
-import { createNewProduct, getProduct } from '../controllers/product.controller.js';
+import { createNewProduct, deleteProduct, getProduct, updateProduct } from '../controllers/product.controller.js';
 
 const productRouter = Router();
 
 // Create a new product
-productRouter.post('/', async(req, res) =>{
+productRouter.post('/', async (req, res) => {
     createNewProduct(req, res);
 });
 
+// For delete a product
+productRouter.delete('/', async (req, res) => {
+    deleteProduct(req, res);
+});
+
 // For getting a product information
-productRouter.get('/:id', async (req, res) =>{
+productRouter.get('/:id', async (req, res) => {
     getProduct(req, res);
+});
+
+// For update all product information
+productRouter.patch('/:id', async (req, res) => {
+    updateProduct(req, res);
 });
 
 export default productRouter;
