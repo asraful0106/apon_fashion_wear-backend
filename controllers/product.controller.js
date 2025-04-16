c32ad2a188d02234e2e043d1c58d99e9996c4c48
Md. Asraful Alom <asrafulapf@gmail.com>
2025-04-16 06:33:36 +0600

    product post request has been done

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -3,7 +3,7 @@ import crypto from 'crypto'; // For fixed length uuid
 
 const prisma = new PrismaClient();
 
-// For getting the product details
+// For getting the product details GET request
 const getProduct = async (req, res) => {
     const product_id = req.params.id;
     if (!product_id) {
@@ -47,20 +47,24 @@ const getProduct = async (req, res) => {
     }
 }
 
-// create product
+// create product POST request
 const createNewProduct = async (req, res) => {
     const {
         name,
         description,
         price,
+        offer_price, // otpional
+        accept_preorder, // optional
+        is_fetured,  // optional
         category_id,
-        sub_category_id,
+        sub_category_id,  // optional
+        colors, // Arrays of colors with images and inventories
     } = req.body;
 
     // Fixed Length UUID for product
     const product_id = crypto.randomBytes(10).toString('hex');
 
-    if (!name, !description, !price, !category_id) {
+    if (!name, !description, !price, !category_id, !colors) {
         return res.status(400).json({ "message": "Please fill all the fields!" });
     }
 
@@ -71,9 +75,50 @@ const createNewProduct = async (req, res) => {
                 name,
                 description,
                 price,
+                offer_price: offer_price ?? null,
+                accept_preorder: accept_preorder ?? false,
+                is_fetured: is_fetured ?? false,
                 category_id,
-                sub_category_id: sub_category_id ?? null
-            }
+                sub_category_id: sub_category_id ?? null,
+                colors: {
+                    create: colors.map((color) => ({
+                        name: color.name,
+                        images: {
+                            create: color.images?.map((image) => ({
+                                image_url: image.url,
+                            })) || [], // Handle case where images might be empty
+                        },
+                        inventories: {
+                            create: color.inventories?.map((inventory) => ({
+                                product_quantity: inventory.quantity,
+                                size: {
+                                    connect: { size_id: inventory.size_id },
+                                },
+                            })) || [], // Handle case where inventories might be empty
+                        },
+                    })),
+                },
+            },
+            include: {
+                category: {
+                    select: { category_name: true },
+                },
+                subCategory: {
+                    select: { sub_category_name: true },
+                },
+                colors: {
+                    include: {
+                        images: true,
+                        inventories: {
+                            include: {
+                                size: {
+                                    select: { size_id: true, name: true },
+                                },
+                            },
+                        },
+                    },
+                },
+            },
         });
         res.status(201).json(newProduct);
     } catch (err) {
---
+++ prisma/migrations/20250416001620_is_prmiumed_has_been_added/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `product` ADD COLUMN `is_fetured` BOOLEAN NOT NULL DEFAULT false;
